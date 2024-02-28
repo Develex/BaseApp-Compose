cdfd5257a88937c623506b47b05147b5d3d37404
develex <collinfranckena77@gmail.com>
2024-02-28 14:49:18 +0100

    Splitted settings for auto and manual dark mode. Added Switches for settings. Updated DataStore for the changes.

--- app/src/main/java/com/develex/baseapp/screens/SettingsScreen.kt
+++ app/src/main/java/com/develex/baseapp/screens/SettingsScreen.kt
@@ -1,46 +1,41 @@
 package com.develex.baseapp.screens
 
 import android.util.Log
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.develex.baseapp.DarkTheme
-import com.develex.baseapp.LocalTheme
-import com.develex.baseapp.MainActivity
+import com.alorma.compose.settings.storage.base.rememberBooleanSettingState
 import com.develex.baseapp.MainViewModel
 import com.develex.baseapp.R
-import com.example.compose.AppTheme
+import com.develex.baseapp.predatastore.DataStoreManager
+import com.alorma.compose.settings.ui.SettingsSwitch
+import com.develex.baseapp.predatastore.Settings
+import kotlinx.coroutines.launch
 
 @Composable
 fun SettingsScreen(navController: NavController, vm: MainViewModel) {
 
+    val dataStoreManager: DataStoreManager = DataStoreManager(LocalContext.current)
+    val coroutineScope = rememberCoroutineScope()
+    val themeUserSetting = vm.themeUserSetting.collectAsState()
+    val themeAutoUserSetting = vm.themeAutoUserSetting.collectAsState()
+
+    val darkModeState = rememberBooleanSettingState(themeUserSetting.value)
+    val autoModeState = rememberBooleanSettingState(themeAutoUserSetting.value)
 
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -49,31 +44,56 @@ fun SettingsScreen(navController: NavController, vm: MainViewModel) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(15.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+                .padding(top = 20.dp, start = 20.dp),
+            verticalArrangement = Arrangement.Top
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-                    .padding(horizontal = 15.dp, vertical = 10.dp)
-                    .clip(MaterialTheme.shapes.large)
-            ) {
-//                Column {
-//                    Button(onClick = {
-//                        vm.setThemeUserSetting(MainViewModel.AppThemeEnum.MODE_NIGHT)
-//                    }) {
-//                        Text(text = "Night Mode")
-//                    }
-//                    Button(onClick = {
-//                        vm.setThemeUserSetting(MainViewModel.AppThemeEnum.MODE_DAY)
-//                    }) {
-//                        Text(text = "Day Mode")
-//                    }
-//                }
+            SettingsSwitch(
+                icon = {
+                    if (themeAutoUserSetting.value) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.night_sight_auto_24px),
+                            contentDescription = "Follow System"
+                        )
+                    } else {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.night_sight_auto_off_24px),
+                            contentDescription = "Follow System"
+                        )
+                    }
 
-            }
+                },
+                title = { Text(text = "Systeem voorkeuren") },
+                subtitle = { Text(text = "Gebruik de systeem voorkeuren") },
+                onCheckedChange = { newValue ->
+                    run {
+                        vm.setThemeAutoUserSetting(newValue)
+                        coroutineScope.launch {
+                            dataStoreManager.saveToDataStore(Settings(autoMode = newValue))
+                        }
+                    }
+                },
+                state = autoModeState,
+            )
+            SettingsSwitch(
+                icon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.dark_mode_24px),
+                        contentDescription = "Dark Mode"
+                    )
+                },
+                title = { Text(text = "Dark Modus") },
+                subtitle = { Text(text = "Gebruik Dark Modus") },
+                onCheckedChange = { newValue ->
+                    run {
+                        vm.setThemeUserSetting(newValue)
+                        coroutineScope.launch {
+                            dataStoreManager.saveToDataStore(Settings(darkMode = newValue))
+                        }
+                    }
+                },
+                state = darkModeState,
+                enabled = !themeAutoUserSetting.value,
+            )
         }
     }
 }
